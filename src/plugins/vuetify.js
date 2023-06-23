49d978a2616560efb8838c10e089e673a2d25f07
Laritello <larik97@gmail.com>
2023-06-24 02:53:26 +0300

    Добавлена русская локализация Vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,11 +2,17 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { VDataTable } from 'vuetify/labs/VDataTable'
+import { en, ru } from 'vuetify/locale'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
 
 export default createVuetify({
+  locale: {
+    locale: 'ru',
+    fallback: 'en',
+    messages: { ru, en },
+  },
   components: {
     VDataTable,
   },
